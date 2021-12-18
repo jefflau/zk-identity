4c04162c712f4aadd656286aa127baa61a8287b5
Jeff Lau <jeff-lau@live.com>
2021-12-18 12:48:41 +0800

    Update hardhat

--- contracts/AttestationMinter.sol
+++ contracts/AttestationMinter.sol
@@ -3,7 +3,7 @@ pragma solidity 0.8.10;
 
 // is Verifier, ERC721Mintable
 contract ProofOfDfWinner {
-    function mint(bytes proof, bytes nullifier) external {
+    function mint(bytes calldata proof, bytes calldata nullifier) external {
         // verify proof
         // check that nullifier hasn't been used
         // mint NFT
--- contracts/Token.sol
+++ contracts/Token.sol
@@ -2,11 +2,10 @@
 
 // Solidity files have to start with this pragma.
 // It will be used by the Solidity compiler to validate its version.
-pragma solidity ^0.7.0;
+pragma solidity ^0.8.10;
 
 // We import this library to be able to use console.log
 import "hardhat/console.sol";
-
 
 // This is the main building block for smart contracts.
 contract Token {
