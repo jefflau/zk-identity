226fad7aa4506a081bba22cfcda2a7dca4946da0
Jeff Lau <jeff-lau@live.com>
2021-12-18 12:30:21 +0800

    add build to ignore and add contract

---
+++ contracts/AttestationMinter.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: Unlicense
+pragma solidity 0.8.10;
+
+// is Verifier, ERC721Mintable
+contract ProofOfDfWinner {
+    function mint(bytes proof, bytes nullifier) external {
+        // verify proof
+        // check that nullifier hasn't been used
+        // mint NFT
+    }
+}
