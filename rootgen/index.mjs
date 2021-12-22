c7e19756c4d295304a8ace7c08fbf092d6081ec4
lsankar4033 <lsankar4033@gmail.com>
2021-12-21 20:29:02 -0800

    WIP merkle tree building code

--- rootgen/index.mjs
+++ rootgen/index.mjs
@@ -1,10 +1,14 @@
+import { readFile } from 'fs/promises';
+
 import { buildPoseidon } from 'circomlibjs';
 
 const poseidon = await buildPoseidon();
 
 const NULL_NODE = -1;
 
+// TODO: store hex as
 async function buildTree(winnerHexStrs) {
+  // TODO: don't convert here? only on input to poseidon?
   winners = winnerHexStrs.map(s => Number(s));
   winners.sort((a, b) => a - b);
 
@@ -24,17 +28,20 @@ async function buildTree(winnerHexStrs) {
       let child1Leaves = nodeToLeaves[child1];
       let child2Leaves = child2 == NULL_NODE ? [] : nodeToLeaves[child2];
 
+      // TODO: what to do about NULL_NODE?
       for (const leaf of child1Leaves) {
         leafToPathElements[leaf].push(child2);
         leafToPathIndices[leaf].push(0);
       }
 
+      // TODO: what to do about NULL_NODE?
       for (const leaf of child2Leaves) {
         leafToPathElements[leaf].push(child1);
         leafToPathIndices[leaf].push(1);
       }
 
-      let parent = poseidon([n1, n2]);
+      let parent = poseidon([child1, child2]);
+      // TODO: convert parent to int/hex-str
       nodeToLeaves[parent] = child1Leaves.concat(child2Leaves);
 
       newLevel.push(parent);
@@ -50,6 +57,19 @@ async function buildTree(winnerHexStrs) {
   }
 }
 
+async function getWinners() {
+  const r1Winners = JSON.parse(await readFile(new URL('./data/r1-winners.json', import.meta.url)))
+  const r2Winners = JSON.parse(await readFile(new URL('./data/r2-winners.json', import.meta.url)))
+  const r3Winners = JSON.parse(await readFile(new URL('./data/r3-winners.json', import.meta.url)))
+  const r4Winners = JSON.parse(await readFile(new URL('./data/r4-winners.json', import.meta.url)))
 
-// load all winners (let's just do all for now)
-// write output
+  const allWinners = r1Winners.concat(r2Winners, r3Winners, r4Winners);
+
+  return allWinners.map(w => w['winner']);
+}
+
+
+let winners = await getWinners();
+let tree = await buildTree(winners);
+
+console.log(tree['leafToPathElements'])
