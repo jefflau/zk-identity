0f53b64519c2aa963e16e2c0af20680e1003ce77
Veronica Zheng <veronica.zheng@coinbase.com>
2021-12-23 00:23:05 -0800

    add getSignature and cleaned things a bit

--- frontend/src/components/Dapp.js
+++ frontend/src/components/Dapp.js
@@ -18,7 +18,8 @@ import { TransactionErrorMessage } from "./TransactionErrorMessage";
 import { WaitingForTransactionMessage } from "./WaitingForTransactionMessage";
 import { NoTokensMessage } from "./NoTokensMessage";
 
-import { CalculateProof } from "../helpers/ProofHelper";
+import { getMsgHash, getSignature } from "../helpers/GetSignature";
+import { calculateProof, buildContractCallArgs } from "../helpers/ProofHelper";
 
 // This is the Hardhat Network id, you might change it in the hardhat.config.js
 // Here's a list of network ids https://docs.metamask.io/guide/ethereum-provider.html#properties
@@ -162,40 +163,7 @@ export class Dapp extends React.Component {
     );
   }
 
-  componentWillUnmount() {
-    // We poll the user's balance, so we have to stop doing that when Dapp
-    // gets unmounted
-    this._stopPollingData();
-  }
-
-  // TODO: move this to the right place!!!
-  async _generateProof() {
-    console.log("Calculate Proof");
-    // TODO: dummy input, used for testing
-    const merkleRoot = 1234
-    const nullifier = 10
-    const input = {
-      r: [10, 10, 10],
-      s: [10, 10, 10],
-      msghash: [10, 10, 10],
-      chunkedPubkey: [
-          [10, 10, 10],
-          [10, 10, 10]
-      ],
-      nullifier: nullifier,
-      merklePathElements: [10, 10, 10, 10, 10, 10, 10, 10, 10, 10],
-      merklePathIndices: [0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
-      merkleRoot: merkleRoot
-    }  
-    const proof = await calculateProof(input);
-    const args = buildContractCallArgs(proof, merkleRoot, nullifier)
-    console.log(args)
-  }
-
   async _connectWallet() {
-    // TODO: move this to the right place!!!
-    await this._generateProof();
-
     // This method is run when the user clicks the Connect. It connects the
     // dapp to the user's wallet, and initializes it.
 
@@ -228,7 +196,6 @@ export class Dapp extends React.Component {
     
     // We reset the dapp state if the network is changed
     window.ethereum.on("networkChanged", ([networkId]) => {
-      this._stopPollingData();
       this._resetState();
     });
   }
@@ -247,135 +214,44 @@ export class Dapp extends React.Component {
     // Fetching the token data and the user's balance are specific to this
     // sample project, but you can reuse the same initialization pattern.
     this._intializeEthers();
-    this._getTokenData();
-    this._startPollingData();
   }
 
   async _intializeEthers() {
     // We first initialize ethers by creating a provider using window.ethereum
     this._provider = new ethers.providers.Web3Provider(window.ethereum);
 
-    // When, we initialize the contract using that provider and the token's
-    // artifact. You can do this same thing with your contracts.
-
-    // this._token = new ethers.Contract(
-    //   contractAddress.Token,
-    //   TokenArtifact.abi,
-    //   this._provider.getSigner(0)
-    // );
+    // TODO: move this to the right place!!!
+    await this._generateProof();
   }
 
-  // The next two methods are needed to start and stop polling data. While
-  // the data being polled here is specific to this example, you can use this
-  // pattern to read any data from your contracts.
-  //
-  // Note that if you don't need it to update in near real time, you probably
-  // don't need to poll it. If that's the case, you can just fetch it when you
-  // initialize the app, as we do with the token data.
-  _startPollingData() {
-    this._pollDataInterval = setInterval(() => this._updateBalance(), 1000);
+  // TODO: move this to the right place!!!
+  async _generateProof() {
+    const msgHash = getMsgHash();
+    const sig = await getSignature(this._provider.getSigner(0), msgHash)
+    console.log("msgHash: " + msgHash)
+    console.log("sig.r: " + sig.r)
+    console.log("sig.s: " + sig.s)
 
-    // We run it once immediately so we don't have to wait for it
-    this._updateBalance();
-  }
-
-  _stopPollingData() {
-    clearInterval(this._pollDataInterval);
-    this._pollDataInterval = undefined;
-  }
-
-  // The next two methods just read from the contract and store the results
-  // in the component state.
-  async _getTokenData() {
-    const name = await this._token.name();
-    const symbol = await this._token.symbol();
-
-    this.setState({ tokenData: { name, symbol } });
-  }
-
-  async _updateBalance() {
-    const balance = await this._token.balanceOf(this.state.selectedAddress);
-    this.setState({ balance });
-  }
-
-  // This method sends an ethereum transaction to transfer tokens.
-  // While this action is specific to this application, it illustrates how to
-  // send a transaction.
-  async _transferTokens(to, amount) {
-    // Sending a transaction is a complex operation:
-    //   - The user can reject it
-    //   - It can fail before reaching the ethereum network (i.e. if the user
-    //     doesn't have ETH for paying for the tx's gas)
-    //   - It has to be mined, so it isn't immediately confirmed.
-    //     Note that some testing networks, like Hardhat Network, do mine
-    //     transactions immediately, but your dapp should be prepared for
-    //     other networks.
-    //   - It can fail once mined.
-    //
-    // This method handles all of those things, so keep reading to learn how to
-    // do it.
-
-    try {
-      // If a transaction fails, we save that error in the component's state.
-      // We only save one such error, so before sending a second transaction, we
-      // clear it.
-      this._dismissTransactionError();
-
-      // We send the transaction, and save its hash in the Dapp's state. This
-      // way we can indicate that we are waiting for it to be mined.
-      const tx = await this._token.transfer(to, amount);
-      this.setState({ txBeingSent: tx.hash });
-
-      // We use .wait() to wait for the transaction to be mined. This method
-      // returns the transaction's receipt.
-      const receipt = await tx.wait();
-
-      // The receipt, contains a status flag, which is 0 to indicate an error.
-      if (receipt.status === 0) {
-        // We can't know the exact error that made the transaction fail when it
-        // was mined, so we throw this generic one.
-        throw new Error("Transaction failed");
-      }
-
-      // If we got here, the transaction was successful, so you may want to
-      // update your state. Here, we update the user's balance.
-      await this._updateBalance();
-    } catch (error) {
-      // We check the error code to see if this error was produced because the
-      // user rejected a tx. If that's the case, we do nothing.
-      if (error.code === ERROR_CODE_TX_REJECTED_BY_USER) {
-        return;
-      }
-
-      // Other errors are logged and stored in the Dapp's state. This is used to
-      // show them to the user, and for debugging.
-      console.error(error);
-      this.setState({ transactionError: error });
-    } finally {
-      // If we leave the try/catch, we aren't sending a tx anymore, so we clear
-      // this part of the state.
-      this.setState({ txBeingSent: undefined });
+    console.log("Calculate Proof");
+    // TODO: dummy input, used for testing
+    const merkleRoot = 1234
+    const nullifier = 10
+    const input = {
+      r: [10, 10, 10],
+      s: [10, 10, 10],
+      msghash: [10, 10, 10],
+      chunkedPubkey: [
+          [10, 10, 10],
+          [10, 10, 10]
+      ],
+      nullifier: nullifier,
+      merklePathElements: [10, 10, 10, 10, 10, 10, 10, 10, 10, 10],
+      merklePathIndices: [0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
+      merkleRoot: merkleRoot
     }
-  }
-
-  // This method just clears part of the state.
-  _dismissTransactionError() {
-    this.setState({ transactionError: undefined });
-  }
-
-  // This method just clears part of the state.
-  _dismissNetworkError() {
-    this.setState({ networkError: undefined });
-  }
-
-  // This is an utility method that turns an RPC error into a human readable
-  // message.
-  _getRpcErrorMessage(error) {
-    if (error.data) {
-      return error.data.message;
-    }
-
-    return error.message;
+    const proof = await calculateProof(input);
+    const args = buildContractCallArgs(proof, merkleRoot, nullifier)
+    console.log(args)
   }
 
   // This method resets the state
