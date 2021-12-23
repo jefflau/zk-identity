988fbcdf5f74e75b7628f9e560d6156c911626a5
Veronica Zheng <veronica.zheng@coinbase.com>
2021-12-23 13:22:34 -0800

    HexStrToArray

--- frontend/src/helpers/GetInput.js
+++ frontend/src/helpers/GetInput.js
@@ -2,19 +2,38 @@ import { ethers } from "ethers";
 
 const MESSAGE = "ZK Identity: Dark Forest Winners";
 
-function bigint_to_array(n, k, x) {
-    let mod = 1n
-    for (var idx = 0; idx < n; idx++) {
-        mod = mod * 2n
+// k elements, each n bits, little endian
+function hexStrToArray(n, k, hexStr) {
+    var result = new Array(k).fill(0);
+
+    var hex = hexStr.substring(2);
+    var bitArray = new Array(4 * hex.length);
+
+    var resultIdx = 0;
+    var element = 0;
+    for (var i = 0; i < hex.length; i++) {
+        var it = parseInt(hex[hex.length - 1 - i], 16);
+        for (var j = 0; j < 4; j++) {
+            var idx = i * 4 + j;
+            var bit = +((it & (1 << j)) != 0);
+            bitArray[idx] = bit;
+
+            const idxn = idx % n
+            if (idx != 0 && idxn == 0) {
+                result[resultIdx] = element;
+                resultIdx++;
+                element = 0;
+            }
+            element += bit * 2 ** idxn;
+            // TODO: this element would be too large, what type should we use here?
+        }
     }
 
-    let ret = []
-    var x_temp = x
-    for (var idx = 0; idx < k; idx++) {
-        ret.push(x_temp % mod)
-        x_temp = x_temp / mod
+    if (element != 0) {
+        result[resultIdx] = element;
     }
-    return ret
+
+    return result
 }
 
 export async function getInput(signer) {
@@ -26,35 +45,38 @@ export async function getInput(signer) {
 
     const flatSig = await signer.signMessage(MESSAGE);
     const sig = ethers.utils.splitSignature(flatSig);
+    console.log("sig.r: " + sig.r)
+    console.log("sig.s: " + sig.s)
 
     const pubKey = ethers.utils.recoverPublicKey(msgHashBytes, flatSig);
+    console.log("pubKey: " + pubKey)
 
     // just to double check, this addr should match the sAddr above
     const addr = ethers.utils.computeAddress(ethers.utils.arrayify(pubKey));
     console.log(addr);
 
+    console.log(hexStrToArray(86, 3, sig.r))
+    // const arr = bigint_to_array(1, 3, 512)
+    // console.log(arr)
+
     // TODO:
-    // r: bitint_to_array(86, 3, sig.r)
-    // s: bigint_to_array(86, 3, sig.s)
-    // msgHash: bigint_to_array(86, 3, msgHash)
     // chuckedPubKey: flatternPubKey(86, 3, pubKey)
     // nullifier = Poseidon(sig.r[0])
 
     // TODO: get merkleRoot, merklePathElements, and merklePathIndices
 
-    return {}
-    // return {
-    //     r: bigint_to_array(86, 3, sig.r),
-    //     s: bigint_to_array(86, 3, sig.s),
-    //     msghash: bigint_to_array(86, 3, msgHash),
-    //     chunkedPubkey: [
-    //         [10, 10, 10],
-    //         [10, 10, 10]
-    //     ],
-    //     nullifier: 10,
-    //     merklePathElements: [10, 10, 10, 10, 10, 10, 10, 10, 10, 10],
-    //     merklePathIndices: [0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
-    //     merkleRoot: 1234
-    // }
+    return {
+        r: hexStrToArray(86, 3, sig.r),
+        s: hexStrToArray(86, 3, sig.s),
+        msghash: hexStrToArray(86, 3, msgHash),
+        chunkedPubkey: [
+            [10, 10, 10],
+            [10, 10, 10]
+        ],
+        nullifier: 10,
+        merklePathElements: [10, 10, 10, 10, 10, 10, 10, 10, 10, 10],
+        merklePathIndices: [0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
+        merkleRoot: 1234
+    }
 }
 
